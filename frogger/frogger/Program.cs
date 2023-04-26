ad4946645cb5d7c9ace178e8bf30d11613a9d9fb
Yuriy Petrov <Jurijp2005@gmail.com>
2023-04-26 14:03:56 +0200

    lastcommitsui3

--- frogger/frogger/Program.cs
+++ frogger/frogger/Program.cs
@@ -10,13 +10,19 @@ Texture2D frog = Raylib.LoadTexture("frog-1.png.png"); //laddar upp frog sprite
 Rectangle player = new Rectangle(550, 950, frog.width, frog.height);//skapar player
 
 
-string currentScene = "start"; //start, game, end,
+string currentScene = "start"; //start, game, end, win
 float timerMaxValue = 60;   //100 sekunder för att klara av banan. Maxvalue är tiden när man startar
 float timerCurrentValue = 60;//nuvarande tiden
 int jump = 100; //jump ska vara en ruta stor, så 100px.
 int degrees = 0;//rotera player
 
+Rectangle frogCollider = player; //ändra min rectangle hitbox till mitten av spriten. Ändrar positionen längre ner
+
+
+
 List<Obstacle> cars = new List<Obstacle>(); //lista för bilar
+List<Platform> logs = new List<Platform>(); //lista för platoformer
+List<Water> waters = new List<Water>();     //lista för vatten rectangel.
 
 
 
@@ -27,6 +33,18 @@ cars.Add(new(800)); //andra bilen
 cars.Add(new(600)); // tredje bilen
 cars.Add(new(100)); //fjärde bilen
 cars.Add(new(200)); //femte bilen
+
+
+logs.Add(new(300, 300)); //platformerna
+logs.Add(new(300, 400));
+logs.Add(new(600, 300));
+logs.Add(new(600, 400));
+
+waters.Add(new(1, 300, 300)); //lista för vatten
+waters.Add(new(400, 300, 200));
+waters.Add(new(700, 300, 300));
+
+//________________________________________________________________________________________________
 
 while (Raylib.WindowShouldClose() == false)//medans spelfönster är öppet ska hela koden under köras
 {
@@ -57,7 +75,7 @@ while (Raylib.WindowShouldClose() == false)//medans spelfönster är öppet ska 
         else if (Raylib.IsKeyPressed(KeyboardKey.KEY_D) || Raylib.IsKeyPressed(KeyboardKey.KEY_RIGHT))//höger
         {
             player.x += jump;
-            degrees =  90;     //kolla höger
+            degrees = 90;     //kolla höger
         }
 
         else if (Raylib.IsKeyPressed(KeyboardKey.KEY_A) || Raylib.IsKeyPressed(KeyboardKey.KEY_LEFT))//vänster
@@ -70,7 +88,7 @@ while (Raylib.WindowShouldClose() == false)//medans spelfönster är öppet ska 
         {
             player.y -= jump;
         }
-        if (player.x >= 1000 )
+        if (player.x >= 1000)
         {
             player.x -= jump;
         }
@@ -79,15 +97,37 @@ while (Raylib.WindowShouldClose() == false)//medans spelfönster är öppet ska 
             player.x += jump;
         }
 
+        frogCollider = player;
+        frogCollider.x -= player.width / 2; //flyttar på frogens htibox
+        frogCollider.y -= player.height / 2;
+
+        if (frogCollider.y <= 1)
+        {
+            currentScene = "win";
+        }
+
         foreach (Obstacle c in cars) //execute update metoden för (c) item
         {
-        c.Update();
+            c.Update();
+
+            if (Raylib.CheckCollisionRecs(frogCollider, c.carCollider)) //när frog kolliderar med bilen 
+            {
+                currentScene = "end";    //game over
+            }
+        }
+
+        foreach (Water w in waters)
+        {
+            if (Raylib.CheckCollisionRecs(frogCollider, w.rectWater)) //när frog kolliderar med bilen
+            {
+                currentScene = "end";     //game over
+            }
         }
 
     }
-//____________________________________________________________________________
+    //____________________________________________________________________________
 
-    
+
     else if (currentScene == "start")//ifall scenen är på start då ska
     {
         if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))//går till scen game
@@ -95,11 +135,25 @@ while (Raylib.WindowShouldClose() == false)//medans spelfönster är öppet ska 
             currentScene = "game";
         }
     }
+
     else if (currentScene == "end")//ifall scenen är på end då ska
     {
         if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))//gå till scen game
         {
-            currentScene = "game";  
+            currentScene = "game";
+            timerCurrentValue = timerMaxValue;    //återställer timern till maximala tiden (tid från början)
+            player.x = 550;     //ifall game over jag tar x, y och degrees värde till samma som var från början.
+            player.y = 950;
+            degrees = 0;
+            
+        }
+    }
+
+    else if (currentScene == "win")
+    {
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))//gå till start
+        {
+            currentScene = "start";
             timerCurrentValue = timerMaxValue;    //återställer timern till maximala tiden (tid från början)
             player.x = 550;     //ifall game over jag tar x, y och degrees värde till samma som var från början.
             player.y = 950;
@@ -125,19 +179,29 @@ while (Raylib.WindowShouldClose() == false)//medans spelfönster är öppet ska 
 
         Raylib.DrawText($"Time: {(int)timerCurrentValue}", 1, 930, 40, Color.BLACK);
 
-        Raylib.DrawTexturePro(frog, new Rectangle(0,0,100,100), player, new Vector2(50,50), degrees, Color.WHITE);//(texture, Rectangle source, Rectangle dest, Vector2 origin, rotation, Color)
-
         foreach (Obstacle c in cars)   //execute draw metoden för (c) item
         {
-        c.Draw();
+            c.Draw();
+        }
+        
+        foreach (Platform p in logs) //ritar ut platformen
+        {
+            p.DrawPlatform();
         }
 
+        
+
+        Raylib.DrawTexturePro(frog, new Rectangle(0 + degrees, 0 + degrees, 90, 90), player, new Vector2(45, 45), degrees, Color.WHITE);//(texture, Rectangle source, Rectangle dest, Vector2 origin, rotation, Color)
     }//___________________________________________________________________________________________
 
     else if (currentScene == "start")
     {
         Raylib.DrawText("FROGGER", 430, 350, 36, Color.BLACK);             // ritar upp text
         Raylib.DrawText("Press ENTER to start", 350, 450, 34, Color.BLACK);
+        Raylib.DrawText("Use WASD or arrows to move", 350, 550, 34, Color.BLACK);
+        Raylib.DrawText("Avoid cars and water", 350, 650, 34, Color.BLACK);
+
+
     }
 
     else if (currentScene == "end")
@@ -145,6 +209,13 @@ while (Raylib.WindowShouldClose() == false)//medans spelfönster är öppet ska 
         Raylib.ClearBackground(Color.RED); //röd bakgrund 
         Raylib.DrawText("GAME OVER", 430, 350, 36, Color.BLACK);//text
         Raylib.DrawText("Press ENTER to play again", 320, 450, 34, Color.BLACK);
+
+    }
+    else if (currentScene == "win")
+    {
+        Raylib.ClearBackground(Color.GREEN);//grön backgrund
+        Raylib.DrawText("YOU WON!", 430, 350, 36, Color.BLACK);//text
+        Raylib.DrawText("Press ENTER to go to start screen", 320, 450, 34, Color.BLACK);
 
     }
 
